4482a075048e75c385179234d9a359bac57ed5a0
Raymond Kneipp <raymond@Raymonds-MacBook-Air.local>
2021-04-24 15:21:18 -0400

    Removed alert for meeting and changed footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
 } from "react-router-dom";
 
 import Header from "./Header";
@@ -15,29 +15,27 @@ import About from "./About";
 import YoungEagles from "./YoungEagles";
 import Gallery from "./Gallery";
 import Contact from "./Contact";
-import Alert from "./Alert";
 
 const App = () => {
-	return (
-		<Router>
-			<div className="container">
-				<div className="grid">
-					<Alert text="The December meeting has been cancelled." />
-					<Header />
-					<Side />
-					<Switch>
-						<Route path="/" exact component={Main} />
-						<Route path="/about" component={About} />
-						<Route path="/ye" component={YoungEagles} />
-						<Route path="/gallery" component={Gallery} />
-						<Route path="/contact" component={Contact} />
-						<Redirect to="/" />
-					</Switch>
-					<Footer />
-				</div>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="container">
+        <div className="grid">
+          <Header />
+          <Side />
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/about" component={About} />
+            <Route path="/ye" component={YoungEagles} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/contact" component={Contact} />
+            <Redirect to="/" />
+          </Switch>
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,28 @@
 import React from "react";
-import logo from "../images/rk.svg";
 
 const Footer = () => {
-	return (
-		<div className="footer d-flex justify-content-end align-items-center">
-			<p className="m-0 flex-fill">
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					className="btn btn-yellow rk-link"
-					href="https://raymondkneipp.com/"
-				>
-					<img src={logo} alt="RK" className="rk" />
-					Web Design
-				</a>
-			</p>
-			<a
-				href="https://www.facebook.com/eaachapter174/"
-				className="btn btn-yellow"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<i className="fab fa-2x fa-facebook-square" />
-			</a>
-		</div>
-	);
+  return (
+    <div className="footer d-flex justify-content-end align-items-center">
+      <p className="m-0 flex-fill">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="creator"
+          href="https://raymondkneipp.com/"
+        >
+          Website by Raymond Kneipp
+        </a>
+      </p>
+      <a
+        href="https://www.facebook.com/eaachapter174/"
+        className="btn btn-yellow"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-2x fa-facebook-square" />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
